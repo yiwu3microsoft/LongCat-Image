5380c0681264ee107fdaa51de995b5a5fef2a4ec
Yi Wu <yiwu3@microsoft.com>
2026-04-10 01:26:57 +0000

    update training_data_dpo_pa60k_13k.txt

---
+++ train_examples/edit_dpo/train_pa60k_13k_2GPU.sh
@@ -0,0 +1,14 @@
+export TOKENIZERS_PARALLELISM=False
+export NCCL_DEBUG=INFO
+export NCCL_TIMEOUT=12000
+
+script_dir=$(cd -- "$(dirname -- "$0")" &> /dev/null && pwd -P)
+project_root=$(dirname "$(dirname "$script_dir")")
+echo "script_dir" ${script_dir}
+
+deepspeed_config_file=${project_root}/misc/accelerate_config.yaml
+
+accelerate launch  --mixed_precision bf16 --num_processes 2 --config_file ${deepspeed_config_file} \
+${script_dir}/train_edit_dpo_low_VRAM.py \
+--config ${script_dir}/train_config_dpo_pa60k_13k.yaml \
+--allow_tf32
